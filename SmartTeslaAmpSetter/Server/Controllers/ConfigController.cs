11561d2a03cc8142e93ff7fd1c9347e35de02f20
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-03-18 02:49:38 +0100

    refactor(ConfigController): rename parameter for documentation

--- SmartTeslaAmpSetter/Server/Controllers/ConfigController.cs
+++ SmartTeslaAmpSetter/Server/Controllers/ConfigController.cs
@@ -50,7 +50,7 @@ namespace SmartTeslaAmpSetter.Server.Controllers
         /// Update Car's configuration
         /// </summary>
         /// <param name="carId">Car Id of car to update</param>
-        /// <param name="carConfiguration">Car Configuration which should be set to car</param>
+        /// <param name="carBasicConfiguration">Car Configuration which should be set to car</param>
         [HttpPut]
         public void UpdateCarBasicConfiguration(int carId, [FromBody] CarBasicConfiguration carBasicConfiguration) =>
             _service.UpdateCarBasicConfiguration(carId, carBasicConfiguration);
