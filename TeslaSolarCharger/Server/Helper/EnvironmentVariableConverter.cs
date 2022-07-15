95123f830b42199a04950e68a2e79ac5a2e45191
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-07-15 23:25:40 +0200

    feat(EnvironmentVariableConverter): convert Environmentvariables to json file

---
+++ TeslaSolarCharger/Server/Helper/EnvironmentVariableConverter.cs
@@ -0,0 +1,58 @@
+﻿using TeslaSolarCharger.Shared.Dtos;
+
+namespace TeslaSolarCharger.Server.Helper;
+
+public class EnvironmentVariableConverter
+{
+    private readonly ILogger<EnvironmentVariableConverter> _logger;
+    private readonly IConfiguration _configuration;
+
+    public EnvironmentVariableConverter(ILogger<EnvironmentVariableConverter> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public void ConvertAllValues()
+    {
+        _logger.LogTrace("{method}()", nameof(ConvertAllValues));
+        var dtoBaseConfiguration = new DtoBaseConfiguration()
+        {
+            CurrentPowerToGridUrl = _configuration.GetValue<string>("CurrentPowerToGridUrl"),
+            CurrentInverterPowerUrl = _configuration.GetValue<string?>("CurrentInverterPowerUrl"),
+            TeslaMateApiBaseUrl = _configuration.GetValue<string>("TeslaMateApiBaseUrl"),
+            UpdateIntervalSeconds = _configuration.GetValue<int>("UpdateIntervalSeconds"),
+            PvValueUpdateIntervalSeconds = _configuration.GetValue<int>("PvValueUpdateIntervalSeconds"),
+            GeoFence = _configuration.GetValue<string>("GeoFence"),
+            MinutesUntilSwitchOn = _configuration.GetValue<int>("MinutesUntilSwitchOn"),
+            MinutesUntilSwitchOff = _configuration.GetValue<int>("MinutesUntilSwitchOff"),
+            PowerBuffer = _configuration.GetValue<int>("PowerBuffer"),
+            CurrentPowerToGridJsonPattern = _configuration.GetValue<string?>("CurrentPowerToGridJsonPattern"),
+            CurrentPowerToGridInvertValue = _configuration.GetValue<bool?>("CurrentPowerToGridInvertValue"),
+            CurrentInverterPowerJsonPattern = _configuration.GetValue<string?>("CurrentInverterPowerJsonPattern"),
+            TelegramBotKey = _configuration.GetValue<string?>("TelegramBotKey"),
+            TelegramChannelId = _configuration.GetValue<string?>("TelegramChannelId"),
+            TeslaMateDbServer = _configuration.GetValue<string>("TeslaMateDbServer"),
+            TeslaMateDbPort = _configuration.GetValue<int>("TeslaMateDbPort"),
+            TeslaMateDbDatabaseName = _configuration.GetValue<string>("TeslaMateDbDatabaseName"),
+            TeslaMateDbUser = _configuration.GetValue<string>("TeslaMateDbUser"),
+            TeslaMateDbPassword = _configuration.GetValue<string>("TeslaMateDbPassword"),
+            MqqtClientId = _configuration.GetValue<string>("MqqtClientId"),
+            MosquitoServer = _configuration.GetValue<string>("MosquitoServer"),
+            CurrentPowerToGridXmlPattern = _configuration.GetValue<string?>("CurrentPowerToGridXmlPattern"),
+            CurrentPowerToGridXmlAttributeHeaderName = _configuration.GetValue<string?>("CurrentPowerToGridXmlAttributeHeaderName"),
+            CurrentPowerToGridXmlAttributeHeaderValue = _configuration.GetValue<string?>("CurrentPowerToGridXmlAttributeHeaderValue"),
+            CurrentPowerToGridXmlAttributeValueName = _configuration.GetValue<string?>("CurrentPowerToGridXmlAttributeValueName"),
+            CurrentInverterPowerXmlPattern = _configuration.GetValue<string?>("CurrentInverterPowerXmlPattern"),
+            CurrentInverterPowerXmlAttributeHeaderName = _configuration.GetValue<string?>("CurrentInverterPowerAttributeHeaderName"),
+            CurrentInverterPowerXmlAttributeHeaderValue = _configuration.GetValue<string?>("CurrentInverterPowerAttributeHeaderValue"),
+            CurrentInverterPowerXmlAttributeValueName = _configuration.GetValue<string?>("CurrentInverterPowerAttributeValueName"),
+        };
+        var carPriorities = _configuration.GetValue<string>("");
+        var elements = carPriorities.Split('|').ToList();
+        foreach (var element in elements)
+        {
+            dtoBaseConfiguration.CarPriorities.Add(Convert.ToInt32(element));
+        }
+    }
+}
