84e03595ae5c87fc1609ee3dfa2fcc1af85cfede
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-07-23 14:28:26 +0200

    feat(ModbusController): Clarify documentation

--- Plugins.Modbus/Controllers/ModbusController.cs
+++ Plugins.Modbus/Controllers/ModbusController.cs
@@ -38,7 +38,7 @@ namespace Plugins.Modbus.Controllers
         /// </summary>
         /// <param name="unitIdentifier">The unit identifier is used to communicate via devices such as bridges, routers and gateways that use a single IP address to support multiple independent Modbus end units. Thus, the unit identifier is the address of a remote slave connected on a serial line or on other buses. Use the default values 0x00 or 0xFF when communicating to a Modbus server that is directly connected to a TCP/IP network.</param>
         /// <param name="startingAddress">The holding register start address for the read operation.</param>
-        /// <param name="quantity">The number of holding registers (16 bit per register) to read.</param>
+        /// <param name="quantity">The number of holding registers (16 bit per register) to read. Should be 2 for int32</param>
         /// <param name="ipAddress">The ip address of the modbus device</param>
         /// <param name="port">The modbus port of the modbus device</param>
         /// <param name="factor">The factor to multiply the outcoming modbus value with (e.g. if value is 0.1 W you have to use 10 as factor)</param>
@@ -56,14 +56,14 @@ namespace Plugins.Modbus.Controllers
         /// </summary>
         /// <param name="unitIdentifier">The unit identifier is used to communicate via devices such as bridges, routers and gateways that use a single IP address to support multiple independent Modbus end units. Thus, the unit identifier is the address of a remote slave connected on a serial line or on other buses. Use the default values 0x00 or 0xFF when communicating to a Modbus server that is directly connected to a TCP/IP network.</param>
         /// <param name="startingAddress">The holding register start address for the read operation.</param>
-        /// <param name="quantity">The number of holding registers (16 bit per register) to read.</param>
+        /// <param name="quantity">The number of holding registers (16 bit per register) to read. Should be 1 for int 16.</param>
         /// <param name="ipAddress">The ip address of the modbus device</param>
         /// <param name="port">The modbus port of the modbus device</param>
         /// <param name="factor">The factor to multiply the outcoming modbus value with (e.g. if value is 0.1 W you have to use 10 as factor)</param>
         /// <param name="connectDelaySeconds"></param>
         /// <param name="timeoutSeconds"></param>
         /// <param name="minimumResult">Sets a minimum return result. This ist important, if your inverter does not send 0 as power if it is off.</param>
-        /// <returns>Modbus value converted to Int32</returns>
+        /// <returns>Modbus value converted to Int16</returns>
         [HttpGet]
         public Task<short> GetInt16Value(byte unitIdentifier, ushort startingAddress, ushort quantity, string ipAddress,
             int port, float factor, int connectDelaySeconds, int timeoutSeconds, int? minimumResult = null)
