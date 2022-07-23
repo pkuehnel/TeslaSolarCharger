c8cffae68308744d4fc2c994da23b3835df032dc
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-07-23 14:37:29 +0200

    fix(ModbusService): Make ReadInt16Value async

--- Plugins.Modbus/Services/ModbusService.cs
+++ Plugins.Modbus/Services/ModbusService.cs
@@ -29,7 +29,7 @@ public class ModbusService : IModbusService
         return value;
     }
 
-    public Task<short> ReadInt16Value(byte unitIdentifier, ushort startingAddress, ushort quantity, string ipAddressString, int port,
+    public async Task<short> ReadInt16Value(byte unitIdentifier, ushort startingAddress, ushort quantity, string ipAddressString, int port,
         float factor, int connectDelay, int timeout, int? minimumResult)
     {
         _logger.LogTrace("{method}({unitIdentifier}, {startingAddress}, {quantity}, {ipAddressString}, {port}, {factor}, " +
