088e4b8205cb6071dbdeca6608cde1e4c33214f8
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-07-30 14:30:27 +0200

    fix(BaseConfigurationService): updating configuration did not update jobintervalls

---
+++ TeslaSolarCharger/Server/Services/BaseConfigurationService.cs
@@ -0,0 +1,27 @@
+﻿using TeslaSolarCharger.Server.Contracts;
+using TeslaSolarCharger.Server.Scheduling;
+using TeslaSolarCharger.Shared.Contracts;
+using TeslaSolarCharger.Shared.Dtos.BaseConfiguration;
+
+namespace TeslaSolarCharger.Server.Services;
+
+public class BaseConfigurationService : IBaseConfigurationService
+{
+    private readonly ILogger<BaseConfigurationService> _logger;
+    private readonly IConfigurationWrapper _configurationWrapper;
+    private readonly JobManager _jobManager;
+
+    public BaseConfigurationService(ILogger<BaseConfigurationService> logger, IConfigurationWrapper configurationWrapper, JobManager jobManager)
+    {
+        _logger = logger;
+        _configurationWrapper = configurationWrapper;
+        _jobManager = jobManager;
+    }
+
+    public async Task UpdateBaseConfigurationAsync(DtoBaseConfiguration baseConfiguration)
+    {
+        await _jobManager.StopJobs().ConfigureAwait(false);
+        await _configurationWrapper.UpdateBaseConfigurationAsync(baseConfiguration).ConfigureAwait(false);
+        await _jobManager.StartJobs().ConfigureAwait(false);
+    }
+}
