b319cb9f5d75a3d180c9b3d36587dde362df831c
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-08-02 22:14:01 +0200

    fix(BaseConfigurationService): could not reconnect with different mqtt options

--- TeslaSolarCharger/Server/Services/BaseConfigurationService.cs
+++ TeslaSolarCharger/Server/Services/BaseConfigurationService.cs
@@ -10,18 +10,23 @@ public class BaseConfigurationService : IBaseConfigurationService
     private readonly ILogger<BaseConfigurationService> _logger;
     private readonly IConfigurationWrapper _configurationWrapper;
     private readonly JobManager _jobManager;
+    private readonly IMqttService _mqttService;
 
-    public BaseConfigurationService(ILogger<BaseConfigurationService> logger, IConfigurationWrapper configurationWrapper, JobManager jobManager)
+    public BaseConfigurationService(ILogger<BaseConfigurationService> logger, IConfigurationWrapper configurationWrapper,
+        JobManager jobManager, IMqttService mqttService)
     {
         _logger = logger;
         _configurationWrapper = configurationWrapper;
         _jobManager = jobManager;
+        _mqttService = mqttService;
     }
 
     public async Task UpdateBaseConfigurationAsync(DtoBaseConfiguration baseConfiguration)
     {
+        _logger.LogTrace("{method}({@baseConfiguration})", nameof(UpdateBaseConfigurationAsync), baseConfiguration);
         await _jobManager.StopJobs().ConfigureAwait(false);
         await _configurationWrapper.UpdateBaseConfigurationAsync(baseConfiguration).ConfigureAwait(false);
+        await _mqttService.ConnectMqttClient();
         await _jobManager.StartJobs().ConfigureAwait(false);
     }
 }
