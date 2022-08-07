b57724c8c5ee87103de62b3fdfa9af062a937a73
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-08-07 17:34:20 +0200

    feat(version increment): only increment if running in visual studio

--- TeslaSolarCharger/Server/VersionAutoIncrementer.cs
+++ TeslaSolarCharger/Server/VersionAutoIncrementer.cs
@@ -1,11 +1,11 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.200.919")]
+[assembly: AssemblyVersion("1.0.200.933")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeslaSolarCharger.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.200.919")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.200.919")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.200.933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.200.933")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeslaSolarCharger.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeslaSolarCharger.Client")]
 
