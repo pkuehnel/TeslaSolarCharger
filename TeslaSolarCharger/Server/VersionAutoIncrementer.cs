6aa7e9bc4be6ce6d35f4674efb21db8c1ceae529
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-08-07 17:19:55 +0200

    feat(serverproject): autoincrement version on build

---
+++ TeslaSolarCharger/Server/VersionAutoIncrementer.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.200.919")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TeslaSolarCharger.Client")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.200.919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.200.919")]
+[assembly: System.Reflection.AssemblyProductAttribute("TeslaSolarCharger.Client")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TeslaSolarCharger.Client")]
+
