b1ed1e288705ed357de02ae463d64fef0980500a
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-08-07 19:27:06 +0200

    fix(VersionAutoIncrementer): use correct major and minor

--- TeslaSolarCharger/Server/VersionAutoIncrementer.cs
+++ TeslaSolarCharger/Server/VersionAutoIncrementer.cs
@@ -1,11 +1,11 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.200.954")]
+[assembly: AssemblyVersion("2.3.200.1046")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeslaSolarCharger.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.200.954")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.200.954")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.3.200.1046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.3.200.1046")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeslaSolarCharger.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeslaSolarCharger.Client")]
 
