e1f1bb2f667d01fa8ed9d7be1e60e4b1f6885a76
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-08-15 11:39:21 +0200

    feat(AutoIncrementer): update version to 2.4.1

--- TeslaSolarCharger/Server/VersionAutoIncrementer.cs
+++ TeslaSolarCharger/Server/VersionAutoIncrementer.cs
@@ -1,11 +1,11 @@
 ﻿
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyVersion("2.4.1.0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeslaSolarCharger.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2.4.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.4.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.4.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.4.1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeslaSolarCharger.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeslaSolarCharger.Client")]
 
