62132413fc136735dc80633c4fa721330a70c823
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-09-03 14:50:54 +0200

    feat(IssueValidationService): Create Issue validation service

---
+++ TeslaSolarCharger/Server/Services/IssueValidationService.cs
@@ -0,0 +1,27 @@
+﻿using TeslaSolarCharger.Server.Contracts;
+using TeslaSolarCharger.Shared.Dtos;
+using TeslaSolarCharger.Shared.Dtos.Contracts;
+
+namespace TeslaSolarCharger.Server.Services;
+
+public class IssueValidationService
+{
+    private readonly ILogger<IssueValidationService> _logger;
+    private readonly ITeslaService _teslaService;
+    private readonly IPvValueService _pvValueService;
+    private readonly ISettings _settings;
+
+    public IssueValidationService(ILogger<IssueValidationService> logger,
+        ITeslaService teslaService, IPvValueService pvValueService, ISettings settings)
+    {
+        _logger = logger;
+        _teslaService = teslaService;
+        _pvValueService = pvValueService;
+        _settings = settings;
+    }
+
+    public void RefreshIssues()
+    {
+        var issueList = new List<Issue>();
+    }
+}
---
+++ TeslaSolarCharger/Shared/Dtos/Issue.cs
@@ -0,0 +1,16 @@
+﻿using TeslaSolarCharger.Shared.Enums;
+
+namespace TeslaSolarCharger.Shared.Dtos;
+
+public class Issue
+{
+    public Issue()
+    {
+        IssueCode = string.Empty;
+        IssueMessage = string.Empty;
+    }
+
+    public string IssueCode { get; set; }
+    public string IssueMessage { get; set; }
+    public IssueType? IssueType { get; set; }
+}
