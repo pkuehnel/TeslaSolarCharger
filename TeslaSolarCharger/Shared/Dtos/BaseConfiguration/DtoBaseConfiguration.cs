ef8042264b6dc0b02a8291e9c2c82b14ea6dbbc7
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-09-08 23:51:20 +0200

    fix(dtoBaseConfiguration): always use en-US culture

--- TeslaSolarCharger/Shared/Dtos/BaseConfiguration/DtoBaseConfiguration.cs
+++ TeslaSolarCharger/Shared/Dtos/BaseConfiguration/DtoBaseConfiguration.cs
@@ -7,44 +7,44 @@ public class DtoBaseConfiguration : BaseConfigurationBase
     private string? _currentPowerToGridCorrectionFactorString;
     public string CurrentPowerToGridCorrectionFactorString
     {
-        get => CurrentPowerToGridCorrectionFactor.ToString(CultureInfo.InvariantCulture);
+        get => CurrentPowerToGridCorrectionFactor.ToString(new CultureInfo("en-US"));
         set
         {
             _currentPowerToGridCorrectionFactorString = value.Replace(",", ".");
-            CurrentPowerToGridCorrectionFactor = Convert.ToDecimal(_currentPowerToGridCorrectionFactorString);
+            CurrentPowerToGridCorrectionFactor = Convert.ToDecimal(_currentPowerToGridCorrectionFactorString, new CultureInfo("en-US"));
         }
     }
 
     private string? _currentInverterPowerCorrectionFactorString;
     public string CurrentInverterPowerCorrectionFactorString
     {
-        get => CurrentInverterPowerCorrectionFactor.ToString(CultureInfo.InvariantCulture);
+        get => CurrentInverterPowerCorrectionFactor.ToString(new CultureInfo("en-US"));
         set
         {
             _currentInverterPowerCorrectionFactorString = value.Replace(",", ".");
-            CurrentInverterPowerCorrectionFactor = Convert.ToDecimal(_currentInverterPowerCorrectionFactorString);
+            CurrentInverterPowerCorrectionFactor = Convert.ToDecimal(_currentInverterPowerCorrectionFactorString, new CultureInfo("en-US"));
         }
     }
 
     private string? _homeBatterySocCorrectionFactorString;
     public string HomeBatterySocCorrectionFactorString
     {
-        get => HomeBatterySocCorrectionFactor.ToString(CultureInfo.InvariantCulture);
+        get => HomeBatterySocCorrectionFactor.ToString(new CultureInfo("en-US"));
         set
         {
             _homeBatterySocCorrectionFactorString = value.Replace(",", ".");
-            HomeBatterySocCorrectionFactor = Convert.ToDecimal(_homeBatterySocCorrectionFactorString);
+            HomeBatterySocCorrectionFactor = Convert.ToDecimal(_homeBatterySocCorrectionFactorString, new CultureInfo("en-US"));
         }
     }
 
     private string? _homeBatteryPowerCorrectionFactorString;
     public string HomeBatteryPowerCorrectionFactorString
     {
-        get => HomeBatteryPowerCorrectionFactor.ToString(CultureInfo.InvariantCulture);
+        get => HomeBatteryPowerCorrectionFactor.ToString(new CultureInfo("en-US"));
         set
         {
             _homeBatteryPowerCorrectionFactorString = value.Replace(",", ".");
-            HomeBatteryPowerCorrectionFactor = Convert.ToDecimal(_homeBatteryPowerCorrectionFactorString);
+            HomeBatteryPowerCorrectionFactor = Convert.ToDecimal(_homeBatteryPowerCorrectionFactorString, new CultureInfo("en-US"));
         }
     }
 }
