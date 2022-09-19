a5e767badc00fc2e65cb12de3c398fbdca540573
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-09-19 02:27:51 +0200

    feat(BaseConfigurationService): AutoReconnect solarmqttService

--- TeslaSolarCharger/Server/Services/BaseConfigurationService.cs
+++ TeslaSolarCharger/Server/Services/BaseConfigurationService.cs
@@ -11,14 +11,16 @@ public class BaseConfigurationService : IBaseConfigurationService
     private readonly IConfigurationWrapper _configurationWrapper;
     private readonly JobManager _jobManager;
     private readonly ITeslaMateMqttService _teslaMateMqttService;
+    private readonly ISolarMqttService _solarMqttService;
 
     public BaseConfigurationService(ILogger<BaseConfigurationService> logger, IConfigurationWrapper configurationWrapper,
-        JobManager jobManager, ITeslaMateMqttService teslaMateMqttService)
+        JobManager jobManager, ITeslaMateMqttService teslaMateMqttService, ISolarMqttService solarMqttService)
     {
         _logger = logger;
         _configurationWrapper = configurationWrapper;
         _jobManager = jobManager;
         _teslaMateMqttService = teslaMateMqttService;
+        _solarMqttService = solarMqttService;
     }
 
     public async Task UpdateBaseConfigurationAsync(DtoBaseConfiguration baseConfiguration)
@@ -27,6 +29,7 @@ public class BaseConfigurationService : IBaseConfigurationService
         await _jobManager.StopJobs().ConfigureAwait(false);
         await _configurationWrapper.UpdateBaseConfigurationAsync(baseConfiguration).ConfigureAwait(false);
         await _teslaMateMqttService.ConnectMqttClient().ConfigureAwait(false);
+        await _solarMqttService.ConnectMqttClient().ConfigureAwait(false);
         await _jobManager.StartJobs().ConfigureAwait(false);
     }
 }
