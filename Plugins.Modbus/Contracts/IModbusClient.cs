914ca8d1f9bbced1e834b7a03cb8bd2446c99a73
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2022-10-18 00:21:11 +0200

    refactor(ModbusClient): remove unnecessary code

--- Plugins.Modbus/Contracts/IModbusClient.cs
+++ Plugins.Modbus/Contracts/IModbusClient.cs
@@ -2,14 +2,7 @@
 
 public interface IModbusClient : IDisposable
 {
-    Task<int> ReadInt32Value(byte unitIdentifier, ushort startingAddress, ushort quantity,
-        string ipAddressString,
-        int port, int connectDelay, int timeout, int? minimumResult);
-
     bool DiconnectIfConnected();
-    Task<short> ReadInt16Value(byte unitIdentifier, ushort startingAddress, ushort quantity, string ipAddressString, int port, int connectDelay, int timeout, int? minimumResult);
-    Task<float> ReadFloatValue(byte unitIdentifier, ushort startingAddress, ushort quantity, string ipAddressString, int port, int connectDelay, int timeout, int? minimumResult);
-
     Task<byte[]> GetByteArray(byte unitIdentifier, ushort startingAddress, ushort quantity, string ipAddressString,
         int port, int connectDelay, int timeout);
 }
