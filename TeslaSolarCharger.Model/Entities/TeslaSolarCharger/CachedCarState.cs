7a7bca9c39f27afbdf5d3db760bffab436c68ea2
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2023-01-06 11:53:58 +0100

    fix(CachedCarState): allow cached Car state to be null for new cars

--- TeslaSolarCharger.Model/Entities/TeslaSolarCharger/CachedCarState.cs
+++ TeslaSolarCharger.Model/Entities/TeslaSolarCharger/CachedCarState.cs
@@ -4,6 +4,6 @@ public class CachedCarState
 {
     public int Id { get; set; }
     public int CarId { get; set; }
-    public string CarStateJson { get; set; }
+    public string? CarStateJson { get; set; }
     public DateTime LastUpdated { get; set; }
 }
