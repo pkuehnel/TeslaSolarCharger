68ae4fa270011cfab2e2ed7c8f5f6c7318c7de10
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2023-01-06 13:22:09 +0100

    feat(TeslaSolarChargerContext): Allow CarStateJson to be null

---
+++ TeslaSolarCharger.Model/Migrations/20230106113012_AllowCarStateJsonToBeNull.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeslaSolarCharger.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllowCarStateJsonToBeNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CarStateJson",
+                table: "CachedCarStates",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CarStateJson",
+                table: "CachedCarStates",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
