ff76e55fe75c834751c1cc03bab35ae742936346
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2023-02-11 20:56:04 +0100

    feat(PossibleIssues): Clarify HomeBatteryMinSoc and HomBatteryPower not configured message

--- TeslaSolarCharger/Server/Resources/PossibleIssues/IssueKeys.cs
+++ TeslaSolarCharger/Server/Resources/PossibleIssues/IssueKeys.cs
@@ -10,7 +10,6 @@ public class IssueKeys
     public string HomeBatterySocNotAvailable = "HomeBatterySocNotAvailable";
     public string HomeBatterySocNotPlausible = "HomeBatterySocNotPlausible";
     public string HomeBatteryPowerNotAvailable = "HomeBatteryPowerNotAvailable";
-    public string HomeBatteryHalfConfigured = "HomeBatteryHalfConfigured";
     public string HomeBatteryMinimumSocNotConfigured = "HomeBatteryMinimumSocNotConfigured";
     public string HomeBatteryChargingPowerNotConfigured = "HomeBatteryChargingPowerNotConfigured";
     public string TeslaMateApiNotAvailable = "TeslaMateApiNotAvailable";
