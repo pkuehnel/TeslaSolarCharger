439b49de18cb86a8e9c86e4d30762835dd5dd25b
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2023-03-25 13:31:09 +0100

    feat(CachedCarState): add Key

--- TeslaSolarCharger.Model/Entities/TeslaSolarCharger/CachedCarState.cs
+++ TeslaSolarCharger.Model/Entities/TeslaSolarCharger/CachedCarState.cs
@@ -4,6 +4,7 @@ public class CachedCarState
 {
     public int Id { get; set; }
     public int CarId { get; set; }
+    public string Key { get; set; }
     //Needs to be nullable for new/unused cars
     public string? CarStateJson { get; set; }
     public DateTime LastUpdated { get; set; }
