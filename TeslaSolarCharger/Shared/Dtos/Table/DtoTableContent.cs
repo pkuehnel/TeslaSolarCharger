2d04c2ef532d1018bc259fb0382edae314293812
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2023-03-27 20:00:54 +0200

    feat(tableComponent): use DtoTableContent

---
+++ TeslaSolarCharger/Shared/Dtos/Table/DtoTableContent.cs
@@ -0,0 +1,7 @@
+﻿namespace TeslaSolarCharger.Shared.Dtos.Table;
+
+public class DtoTableContent
+{
+    public DtoTableRow TableHeader { get; set; } = new();
+    public List<DtoTableRow> TableData { get; set; } = new();
+}
