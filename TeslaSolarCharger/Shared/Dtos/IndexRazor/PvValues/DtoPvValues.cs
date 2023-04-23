8b65e6fe776b8497daab72428969350b78db39fb
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2023-04-23 14:47:10 +0200

    feat(IndexRazor): Display power buffer ui

--- TeslaSolarCharger/Shared/Dtos/IndexRazor/PvValues/DtoPvValues.cs
+++ TeslaSolarCharger/Shared/Dtos/IndexRazor/PvValues/DtoPvValues.cs
@@ -7,6 +7,7 @@ public class DtoPvValues
     public int? GridPower { get; set; }
     public int? HomeBatteryPower { get; set; }
     public int? HomeBatterySoc { get; set; }
+    public int? PowerBuffer { get; set; }
     public int? CarCombinedChargingPowerAtHome { get; set; }
     public DateTimeOffset? LastUpdated { get; set; }
 }
