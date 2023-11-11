75833156d7f1d4cc7d1ecb163a77348103eba3cc
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2023-11-11 15:07:06 +0100

    feat(ChargePrice): make Grid Price optional as is not used for other energy providers

---
+++ TeslaSolarCharger.Model/Migrations/20231111140619_MakeGridPriceOptional.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeslaSolarCharger.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeGridPriceOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "GridPrice",
+                table: "ChargePrices",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "GridPrice",
+                table: "ChargePrices",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
