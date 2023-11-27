5f7d3d39fce16926c121faf63a8e5729e870219a
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2023-11-27 21:10:24 +0100

    feat(TeslaFleetApi): Implement charge start, stop and amp set

---
+++ TeslaSolarCharger/Server/Dtos/DtoVehicleCommandResult.cs
@@ -0,0 +1,7 @@
+﻿namespace TeslaSolarCharger.Server.Dtos;
+
+public class DtoVehicleCommandResult
+{
+    bool Result { get; set; }
+    string Reason { get; set; }
+}
--- TeslaSolarCharger/Server/Services/TeslaFleetApiService.cs
+++ TeslaSolarCharger/Server/Services/TeslaFleetApiService.cs
@@ -1,11 +1,13 @@
 ﻿using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
 using TeslaSolarCharger.Model.Contracts;
 using TeslaSolarCharger.Model.Entities.TeslaSolarCharger;
 using TeslaSolarCharger.Server.Contracts;
 using TeslaSolarCharger.Server.Dtos;
 using TeslaSolarCharger.Server.MappingExtensions;
 using TeslaSolarCharger.Shared.Contracts;
+using TeslaSolarCharger.Shared.Dtos.Settings;
 using TeslaSolarCharger.Shared.Enums;
 
 namespace TeslaSolarCharger.Server.Services;
@@ -13,44 +15,74 @@ namespace TeslaSolarCharger.Server.Services;
 public class TeslaFleetApiService : ITeslaService
 {
     private readonly ILogger<TeslaFleetApiService> _logger;
-    private readonly ITeslaSolarChargerContext _context;
+    private readonly ITeslaSolarChargerContext _teslaSolarChargerContext;
     private readonly IMapperConfigurationFactory _mapperConfigurationFactory;
     private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly ITeslamateContext _teslamateContext;
 
-    public TeslaFleetApiService(ILogger<TeslaFleetApiService> logger, ITeslaSolarChargerContext context,
-        IMapperConfigurationFactory mapperConfigurationFactory, IDateTimeProvider dateTimeProvider)
+    private readonly string _chargeStartComand = "command/charge_start";
+    private readonly string _chargeStopComand = "command/charge_stop";
+    private readonly string _setChargingAmps = "command/set_charging_amps";
+    private readonly string _wakeUpComand = "wake_up";
+
+    public TeslaFleetApiService(ILogger<TeslaFleetApiService> logger, ITeslaSolarChargerContext teslaSolarChargerContext,
+        IMapperConfigurationFactory mapperConfigurationFactory, IDateTimeProvider dateTimeProvider, ITeslamateContext teslamateContext)
     {
         _logger = logger;
-        _context = context;
+        _teslaSolarChargerContext = teslaSolarChargerContext;
         _mapperConfigurationFactory = mapperConfigurationFactory;
         _dateTimeProvider = dateTimeProvider;
+        _teslamateContext = teslamateContext;
     }
 
     public async Task StartCharging(int carId, int startAmp, CarStateEnum? carState)
     {
         _logger.LogTrace("{method}({carId}, {startAmp}, {carState})", nameof(StartCharging), carId, startAmp, carState);
         var token = await GetTeslaTokenAsync().ConfigureAwait(false);
-        throw new NotImplementedException();
+        var id = await _teslamateContext.Cars.Where(c => c.Id == carId).Select(c => c.Eid).FirstAsync().ConfigureAwait(false);
+        var result = await SendCommandToTeslaApi(token, id, _chargeStartComand).ConfigureAwait(false);
     }
 
-    public Task WakeUpCar(int carId)
+
+    public async Task WakeUpCar(int carId)
     {
-        throw new NotImplementedException();
+        _logger.LogTrace("{method}({carId})", nameof(WakeUpCar), carId);
+        var token = await GetTeslaTokenAsync().ConfigureAwait(false);
+        var id = await _teslamateContext.Cars.Where(c => c.Id == carId).Select(c => c.Eid).FirstAsync().ConfigureAwait(false);
+        var result = await SendCommandToTeslaApi(token, id, _wakeUpComand).ConfigureAwait(false);
     }
 
-    public Task StopCharging(int carId)
+    public async Task StopCharging(int carId)
     {
-        throw new NotImplementedException();
+        _logger.LogTrace("{method}({carId})", nameof(StopCharging), carId);
+        var token = await GetTeslaTokenAsync().ConfigureAwait(false);
+        var id = await _teslamateContext.Cars.Where(c => c.Id == carId).Select(c => c.Eid).FirstAsync().ConfigureAwait(false);
+        var result = await SendCommandToTeslaApi(token, id, _chargeStopComand).ConfigureAwait(false);
     }
 
-    public Task SetAmp(int carId, int amps)
+    public async Task SetAmp(int carId, int amps)
     {
-        throw new NotImplementedException();
+        _logger.LogTrace("{method}({carId}, {amps})", nameof(SetAmp), carId, amps);
+        var token = await GetTeslaTokenAsync().ConfigureAwait(false);
+        var id = await _teslamateContext.Cars.Where(c => c.Id == carId).Select(c => c.Eid).FirstAsync().ConfigureAwait(false);
+        var commandData = $"{{\"charging_amps\":{amps}}}";
+        var result = await SendCommandToTeslaApi(token, id, _setChargingAmps, commandData).ConfigureAwait(false);
     }
 
     public Task SetScheduledCharging(int carId, DateTimeOffset? chargingStartTime)
     {
-        throw new NotImplementedException();
+        _logger.LogError("This is currently not supported with Fleet API");
+        return Task.CompletedTask;
+    }
+
+    private static async Task<DtoVehicleCommandResult?> SendCommandToTeslaApi(DtoTeslaToken token, long id, string commandName, string contentData = "{}")
+    {
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+        var content = new StringContent(contentData, System.Text.Encoding.UTF8, "application/json");
+        var requestUri = $"https://fleet-api.prd.eu.vn.cloud.tesla.com/api/1/vehicles/{id}/{commandName}";
+        var response = await httpClient.PostAsync(requestUri, content).ConfigureAwait(false);
+        return await response.Content.ReadFromJsonAsync<DtoVehicleCommandResult>().ConfigureAwait(false);
     }
 
     private async Task<DtoTeslaToken> GetTeslaTokenAsync()
@@ -61,11 +93,21 @@ public class TeslaFleetApiService : ITeslaService
                 ;
         });
         var currentDateTime = _dateTimeProvider.UtcNow();
-        var token = await _context.TeslaTokens
+        var token = await _teslaSolarChargerContext.TeslaTokens
             .Where(t => t.ExpiresAtUtc > currentDateTime)
             .OrderByDescending(t => t.ExpiresAtUtc)
             .ProjectTo<DtoTeslaToken>(mapper)
             .FirstAsync().ConfigureAwait(false);
         return token;
     }
+
+    //Just to look up code, not used.
+    //private async Task GetAllAccountVehicles(DtoTeslaToken token)
+    //{
+    //    using var httpClient = new HttpClient();
+    //    var request = new HttpRequestMessage(HttpMethod.Get, $"https://fleet-api.prd.eu.vn.cloud.tesla.com/api/1/vehicles");
+    //    request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+    //    var vehicleResponse = await httpClient.SendAsync(request).ConfigureAwait(false);
+    //    var responseString = await vehicleResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+    //}
 }
