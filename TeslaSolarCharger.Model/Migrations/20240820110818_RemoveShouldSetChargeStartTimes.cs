26e1f3b8d2b5550a42a21aebdcda4b2f34995a5d
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2024-08-20 13:10:01 +0200

    feat(EF): add migration removing ShouldSetChargeStartTimes

---
+++ TeslaSolarCharger.Model/Migrations/20240820110818_RemoveShouldSetChargeStartTimes.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeslaSolarCharger.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveShouldSetChargeStartTimes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShouldSetChargeStartTimes",
+                table: "Cars");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IgnoreLatestTimeToReachSocDateOnWeekend",
+                table: "Cars",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IgnoreLatestTimeToReachSocDateOnWeekend",
+                table: "Cars");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShouldSetChargeStartTimes",
+                table: "Cars",
+                type: "INTEGER",
+                nullable: true);
+        }
+    }
+}
