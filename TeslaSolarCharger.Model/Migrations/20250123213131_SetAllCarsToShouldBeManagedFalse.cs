ff6cd986dc3988a4ca5076136b1cbf40118e3f1d
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-01-23 22:36:25 +0100

    feat(Migrations): Set all cars to should be managed false

---
+++ TeslaSolarCharger.Model/Migrations/20250123213131_SetAllCarsToShouldBeManagedFalse.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeslaSolarCharger.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetAllCarsToShouldBeManagedFalse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE Cars SET ShouldBeManaged = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
