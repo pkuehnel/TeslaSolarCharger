cb2bc2398de5815712243e4e1233057469f250c2
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-01-26 18:39:51 +0100

    fix(TeslaFleetApiService): allow fleet api test even on non licensed fleet api

--- TeslaSolarCharger/Server/Contracts/ITeslaService.cs
+++ TeslaSolarCharger/Server/Contracts/ITeslaService.cs
@@ -1,12 +1,10 @@
-﻿using TeslaSolarCharger.Shared.Dtos;
-using TeslaSolarCharger.Shared.Enums;
+﻿using TeslaSolarCharger.Shared.Enums;
 
 namespace TeslaSolarCharger.Server.Contracts;
 
 public interface ITeslaService
 {
     Task StartCharging(int carId, int startAmp, CarStateEnum? carState);
-    Task WakeUpCar(int carId);
     Task StopCharging(int carId);
     Task SetAmp(int carId, int amps);
 }
