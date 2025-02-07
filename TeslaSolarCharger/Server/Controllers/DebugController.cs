6b2fb7f8a8beb3e1099ba81d20686d26f9bf45d8
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-02-07 22:11:03 +0100

    feat(Serilog): add in Memory sink and configure logging in code

---
+++ TeslaSolarCharger/Server/Controllers/DebugController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PkSoftwareService.Custom.Backend;
+using TeslaSolarCharger.SharedBackend.Abstracts;
+
+namespace TeslaSolarCharger.Server.Controllers;
+
+public class DebugController(InMemorySink inMemorySink, Serilog.Core.LoggingLevelSwitch inMemoryLogLevelSwitch/*chaning log level switch is not tested*/) : ApiBaseController
+{
+    [HttpGet]
+    public IActionResult GetLogs()
+    {
+        var logs = inMemorySink.GetLogs();
+        return Ok(logs);
+    }
+}
