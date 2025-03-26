b17a4dbea3072dc9f0d5e1f11f1ab6176b5dd4c3
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-03-26 22:30:58 +0100

    feat(MeterValueLogService): log PV values

--- TeslaSolarCharger.Model/Entities/TeslaSolarCharger/MeterValue.cs
+++ TeslaSolarCharger.Model/Entities/TeslaSolarCharger/MeterValue.cs
@@ -1,9 +1,12 @@
-﻿namespace TeslaSolarCharger.Model.Entities.TeslaSolarCharger;
+﻿using TeslaSolarCharger.Model.Enums;
+
+namespace TeslaSolarCharger.Model.Entities.TeslaSolarCharger;
 
 public class MeterValue
 {
     public int Id { get; set; }
     public DateTimeOffset Timestamp { get; set; }
+    public MeterValueKind MeterValueKind { get; set; }
     public int? MeasuredPower { get; set; }
     public int? MeasuredEnergy { get; set; }
     public int? EstimatedPower { get; set; }
