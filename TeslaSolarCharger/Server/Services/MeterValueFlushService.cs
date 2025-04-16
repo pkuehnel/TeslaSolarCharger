b0afecb46d060534c6c50ac0913275b110c59e83
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-04-16 10:36:08 +0200

    feat(MeterValueFlushService): saving meter values before shutdown

---
+++ TeslaSolarCharger/Server/Services/MeterValueFlushService.cs
@@ -0,0 +1,19 @@
+﻿using TeslaSolarCharger.Server.Services.Contracts;
+
+namespace TeslaSolarCharger.Server.Services;
+
+public class MeterValueFlushService(ILogger<MeterValueFlushService> logger, IMeterValueLogService meterValueLogService) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        logger.LogTrace("{method}()", nameof(StopAsync));
+        logger.LogInformation("Application is stopping. Flushing buffered meter values to the database.");
+        await meterValueLogService.SaveBufferedMeterValuesToDatabase().ConfigureAwait(false);
+        logger.LogInformation("Flushed buffered meter values to the database.");
+    }
+}
