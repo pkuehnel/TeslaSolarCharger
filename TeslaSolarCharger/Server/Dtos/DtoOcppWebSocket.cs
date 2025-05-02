9db2750c9b37684b7fa76742fa0605fb9f7e0419
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-05-02 21:12:36 +0200

    feat(OcppWebSocketConnectionHandlingService): can send and receive messages

---
+++ TeslaSolarCharger/Server/Dtos/DtoOcppWebSocket.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.WebSockets;
+
+namespace TeslaSolarCharger.Server.Dtos;
+
+public class DtoOcppWebSocket(
+    string chargePointId,
+    WebSocket webSocket,
+    TaskCompletionSource<object?> lifetimeTsc)
+{
+    public string ChargePointId { get; set; } = chargePointId;
+    public WebSocket WebSocket { get; set; } = webSocket;
+    public TaskCompletionSource<object?> LifetimeTsc { get; set; } = lifetimeTsc;
+}
---
+++ TeslaSolarCharger/Server/Services/Contracts/IOcppWebSocketConnectionHandlingService.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.WebSockets;
+
+namespace TeslaSolarCharger.Server.Services.Contracts;
+
+public interface IOcppWebSocketConnectionHandlingService
+{
+
+    void AddWebSocket(string chargePointId,
+        WebSocket webSocket,
+        TaskCompletionSource<object?> lifetimeTcs);
+
+    /// <summary>Send raw bytes to a single charge‑point.</summary>
+    Task SendTextAsync(string chargePointId, string message,
+        CancellationToken ct = default);
+
+    void CleanupDeadConnections();
+}
