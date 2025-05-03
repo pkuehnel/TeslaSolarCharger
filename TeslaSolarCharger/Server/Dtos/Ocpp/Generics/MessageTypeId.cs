358eaeafdfb3b08c7a0d9ca903f8064132c1fca4
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-05-03 22:07:42 +0200

    refactor(ocpp): use another approach for serialization and deserialization

---
+++ TeslaSolarCharger/Server/Dtos/Ocpp/Generics/MessageTypeId.cs
@@ -0,0 +1,11 @@
+﻿namespace TeslaSolarCharger.Server.Dtos.Ocpp.Generics;
+
+/// <summary>
+/// Values defined by the OCPP 1.6 specification (table 11).
+/// </summary>
+public enum MessageTypeId
+{
+    Call = 2,
+    CallResult = 3,
+    CallError = 4,
+}
