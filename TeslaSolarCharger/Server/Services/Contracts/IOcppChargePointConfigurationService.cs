da7f8dc0ed463c763bdeb4bb61069fc856d3db06
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-05-07 04:40:48 +0200

    feat(OcppChargePointConfigurationService): can get and set charge point configurations

---
+++ TeslaSolarCharger/Server/Services/Contracts/IOcppChargePointConfigurationService.cs
@@ -0,0 +1,17 @@
+﻿using TeslaSolarCharger.Server.Dtos;
+using TeslaSolarCharger.Server.Dtos.Ocpp;
+
+namespace TeslaSolarCharger.Server.Services.Contracts;
+
+public interface IOcppChargePointConfigurationService
+{
+    Task<Result<GetConfigurationResponse>> GetOcppConfigurations(string chargepointId, CancellationToken cancellationToken);
+    Task<Result<ChangeConfigurationResponse>> SetOcppConfiguration(string chargepointId, string key, string value,
+        CancellationToken cancellationToken);
+
+    Task<Result<ChangeConfigurationResponse>> SetMeterValuesSampledDataConfiguration(string chargePointId,
+        CancellationToken cancellationToken);
+
+    Task<Result<ChangeConfigurationResponse>> SetMeterValuesSampleIntervalConfiguration(string chargePointId,
+        CancellationToken cancellationToken);
+}
