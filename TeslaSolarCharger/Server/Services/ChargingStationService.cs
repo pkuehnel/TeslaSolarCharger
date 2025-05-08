22f0aed38f139eeb395dabb1def224f85a95e777
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-05-08 13:23:59 +0200

    feat(UI): add elements for Charging station configuration

---
+++ TeslaSolarCharger/Server/Services/ChargingStationService.cs
@@ -0,0 +1,13 @@
+﻿namespace TeslaSolarCharger.Server.Services;
+
+public class ChargingStationService
+{
+    private readonly ILogger<ChargingStationService> _logger;
+
+    public ChargingStationService(ILogger<ChargingStationService> logger)
+    {
+        _logger = logger;
+    }
+
+
+}
