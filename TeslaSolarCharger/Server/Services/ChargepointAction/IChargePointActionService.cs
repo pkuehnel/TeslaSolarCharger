7442e647ba626fa5f0592c33b5449ff9d19315e8
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-05-17 20:18:20 +0200

    feat(SupportRazor): can start ocpp Charging via support page

--- TeslaSolarCharger/Server/Services/ChargepointAction/IChargePointActionService.cs
+++ TeslaSolarCharger/Server/Services/ChargepointAction/IChargePointActionService.cs
@@ -1,10 +1,13 @@
 ﻿using TeslaSolarCharger.Server.Dtos;
+using TeslaSolarCharger.Server.Dtos.Ocpp;
 
 namespace TeslaSolarCharger.Server.Services.ChargepointAction;
 
 public interface IChargePointActionService
 {
-    Task<Result<object?>> StartCharging(string chargepointIdentifier, decimal currentToSet, int numberOfPhases, CancellationToken cancellationToken);
+    Task<Result<RemoteStartTransactionResponse?>> StartCharging(string chargepointIdentifier, decimal currentToSet, int? numberOfPhases,
+        CancellationToken cancellationToken);
     Task<Result<object?>> StopCharging(string chargepointIdentifier, CancellationToken cancellationToken);
-    Task<Result<object?>> SetChargingCurrent(string chargepointIdentifier, decimal currentToSet, int numberOfPhases, CancellationToken cancellationToken);
+    Task<Result<object?>> SetChargingCurrent(string chargepointIdentifier, decimal currentToSet, int? numberOfPhases,
+        CancellationToken cancellationToken);
 }
