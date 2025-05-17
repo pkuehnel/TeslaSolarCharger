a66b973935438bed57924f544c9145794f411de2
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-05-17 19:04:16 +0200

    feat(ChargingStationsRazor): add configuration UI

--- TeslaSolarCharger/Shared/Dtos/ChargingStation/DtoChargingStationConnector.cs
+++ TeslaSolarCharger/Shared/Dtos/ChargingStation/DtoChargingStationConnector.cs
@@ -1,4 +1,6 @@
 ﻿using FluentValidation;
+using System.ComponentModel;
+using TeslaSolarCharger.Shared.Attributes;
 
 namespace TeslaSolarCharger.Shared.Dtos.ChargingStation;
 
@@ -6,8 +8,11 @@ public class DtoChargingStationConnector
 {
     public int Id { get; set; }
     public int ChargingStationId { get; set; }
+    public int ConnectorId { get; set; }
+    [DisplayName("Auto switch between 1 and 3 phases")]
+    [HelperText("When enabled the charger can automatically switch between a 1 and 3 phase charge. Note: Most of the chargers do not support this and some cars might get a hardware damage if enabled, so enable with care.")]
     public bool AutoSwitchBetween1And3PhasesEnabled { get; set; }
-    public int? MinCurrent { get; set; } = 6;
+    public int? MinCurrent { get; set; }
     public int? MaxCurrent { get; set; }
 }
 
@@ -16,7 +21,10 @@ public class ChargingStationConnectorValidator : AbstractValidator<DtoChargingSt
 {
     public ChargingStationConnectorValidator()
     {
-        RuleFor(x => x.MaxCurrent).GreaterThanOrEqualTo(6);
+        RuleFor(x => x.MaxCurrent)
+            .NotEmpty();
+        RuleFor(x => x.MinCurrent)
+            .NotEmpty();
         RuleFor(x => x)
             .Must(config => config.MaxCurrent >= config.MinCurrent)
             .WithMessage("Max Current must be greater than or equal to Min Current.");
