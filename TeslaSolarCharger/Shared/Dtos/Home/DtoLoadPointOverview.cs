8ebde06ae57338bba368d03832306d6cd42bece3
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-05-22 21:45:35 +0200

    feat(HomeService): can get active Loadpoints

--- TeslaSolarCharger/Shared/Dtos/Home/DtoLoadPointOverview.cs
+++ TeslaSolarCharger/Shared/Dtos/Home/DtoLoadPointOverview.cs
@@ -7,8 +7,8 @@ public class DtoLoadPointOverview
     public int? ChargingConnectorId { get; set; }
     public string? ChargingConnectorName { get; set; }
     public int ChargingPower { get; set; }
-    public int MaxPhaseCount { get; set; }
+    public int? MaxPhaseCount { get; set; }
     public int? ChargingPhaseCount { get; set; }
     public int MaxCurrent { get; set; }
-    public int ChargingCurrent { get; set; }
+    public decimal ChargingCurrent { get; set; }
 }
