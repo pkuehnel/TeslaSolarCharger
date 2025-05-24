c1f098f97b6064666a12815d3049d877c12637dd
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-05-24 20:34:04 +0200

    feat(ChargingScheduleComponent): can display car charging schedules

--- TeslaSolarCharger/Server/Services/Contracts/IHomeService.cs
+++ TeslaSolarCharger/Server/Services/Contracts/IHomeService.cs
@@ -1,8 +1,11 @@
-﻿using TeslaSolarCharger.Shared.Dtos.Home;
+﻿using TeslaSolarCharger.Client.Dtos;
+using TeslaSolarCharger.Shared.Dtos.Home;
 
 namespace TeslaSolarCharger.Server.Services.Contracts;
 
 public interface IHomeService
 {
     Task<List<DtoLoadPointOverview>> GetLoadPointOverviews();
+    Task<List<DtoCarChargingSchedule>> GetCarChargingSchedules(int carId);
+    Task<Result<int>> SaveCarChargingSchedule(int carId, DtoCarChargingSchedule dto);
 }
