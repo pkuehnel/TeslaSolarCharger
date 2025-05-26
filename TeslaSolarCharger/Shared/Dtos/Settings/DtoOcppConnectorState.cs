5e82217863ba9be1ce9370dde0ab57b9dd52239c
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-05-26 11:04:20 +0200

    refactor(HomeService): reduce duplicate code

--- TeslaSolarCharger/Shared/Dtos/Settings/DtoOcppConnectorState.cs
+++ TeslaSolarCharger/Shared/Dtos/Settings/DtoOcppConnectorState.cs
@@ -5,8 +5,8 @@ public class DtoOcppConnectorState
     public DtoTimeStampedValue<bool> IsCharging { get; init; } = new(DateTimeOffset.MinValue, false);
     public DtoTimeStampedValue<bool> IsPluggedIn { get; init; } = new(DateTimeOffset.MinValue, false);
     public DtoTimeStampedValue<bool?> IsCarFullyCharged { get; init; } = new(DateTimeOffset.MinValue, null);
-    public DtoTimeStampedValue<int> ChargingPower { get; init; } = new DtoTimeStampedValue<int>(DateTimeOffset.MinValue, 0);
-    public DtoTimeStampedValue<decimal> ChargingCurrent { get; init; } = new DtoTimeStampedValue<decimal>(DateTimeOffset.MinValue, 0);
-    public DtoTimeStampedValue<decimal> ChargingVoltage { get; init; } = new DtoTimeStampedValue<decimal>(DateTimeOffset.MinValue, 0);
-    public DtoTimeStampedValue<int?> PhaseCount { get; init; } = new DtoTimeStampedValue<int?>(DateTimeOffset.MinValue, null);
+    public DtoTimeStampedValue<int> ChargingPower { get; init; } = new(DateTimeOffset.MinValue, 0);
+    public DtoTimeStampedValue<decimal> ChargingCurrent { get; init; } = new(DateTimeOffset.MinValue, 0);
+    public DtoTimeStampedValue<decimal> ChargingVoltage { get; init; } = new(DateTimeOffset.MinValue, 0);
+    public DtoTimeStampedValue<int?> PhaseCount { get; init; } = new(DateTimeOffset.MinValue, null);
 }
