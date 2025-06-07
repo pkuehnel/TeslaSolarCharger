fa9098bebc54c9a75f0f12396b3010b323cc7958
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-06-08 00:58:11 +0200

    feat(LoadpointDetails): can show loadpoint details

---
+++ TeslaSolarCharger/Shared/Dtos/Home/DtoChargingConnectorOverview.cs
@@ -0,0 +1,12 @@
+﻿namespace TeslaSolarCharger.Shared.Dtos.Home;
+
+public class DtoChargingConnectorOverview
+{
+    public DtoChargingConnectorOverview(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+    public bool IsCharging { get; set; }
+}
