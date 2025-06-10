20e3b875e071ee318495b1ad5482bb71fa5d41dd
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-06-11 00:09:40 +0200

    fix(OcppChargingStationConfigurationService): save raw seconds to database

--- TeslaSolarCharger/Server/Services/OcppChargingStationConfigurationService.cs
+++ TeslaSolarCharger/Server/Services/OcppChargingStationConfigurationService.cs
@@ -48,7 +48,7 @@ public class OcppChargingStationConfigurationService(ILogger<OcppChargingStation
                 ShouldBeManaged = cc.ShouldBeManaged,
                 ConnectorId = cc.ConnectorId,
                 AutoSwitchBetween1And3PhasesEnabled = cc.AutoSwitchBetween1And3PhasesEnabled,
-                PhaseSwitchCoolDownTimeSeconds = cc.PhaseSwitchCoolDownTime == null ? null : (int?)cc.PhaseSwitchCoolDownTime.Value.TotalSeconds,
+                PhaseSwitchCoolDownTimeSeconds = cc.PhaseSwitchCoolDownTimeSeconds,
                 MinCurrent = cc.MinCurrent,
                 SwitchOffAtCurrent = cc.SwitchOffAtCurrent,
                 SwitchOnAtCurrent = cc.SwitchOnAtCurrent,
@@ -72,9 +72,7 @@ public class OcppChargingStationConfigurationService(ILogger<OcppChargingStation
         existingChargingStation.MaxCurrent = dtoChargingStation.MaxCurrent;
         existingChargingStation.ConnectedPhasesCount = dtoChargingStation.ConnectedPhasesCount;
         existingChargingStation.AutoSwitchBetween1And3PhasesEnabled = dtoChargingStation.AutoSwitchBetween1And3PhasesEnabled;
-        existingChargingStation.PhaseSwitchCoolDownTime = dtoChargingStation.PhaseSwitchCoolDownTimeSeconds == default
-            ? null
-            : TimeSpan.FromSeconds(dtoChargingStation.PhaseSwitchCoolDownTimeSeconds.Value);
+        existingChargingStation.PhaseSwitchCoolDownTimeSeconds = dtoChargingStation.PhaseSwitchCoolDownTimeSeconds;
         existingChargingStation.ChargingPriority = dtoChargingStation.ChargingPriority;
         await teslaSolarChargerContext.SaveChangesAsync();
     }
