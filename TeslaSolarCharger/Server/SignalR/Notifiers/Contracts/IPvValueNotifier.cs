a64dc9ae710134fa4f86af2269c57720936305b1
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-06-18 03:42:18 +0200

    feat(PowerFlowComponent): use SignalR for getting pv values

---
+++ TeslaSolarCharger/Server/SignalR/Notifiers/Contracts/IPvValueNotifier.cs
@@ -0,0 +1,8 @@
+﻿using TeslaSolarCharger.Shared.Dtos.IndexRazor.PvValues;
+
+namespace TeslaSolarCharger.Server.SignalR.Notifiers.Contracts;
+
+public interface IPvValueNotifier
+{
+    Task NotifyNewValuesAsync(DtoPvValues newValues);
+}
