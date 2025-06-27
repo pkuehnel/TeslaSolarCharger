744a4ab76b507084dc1303ca7ab4182b4808386b
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-06-27 21:53:00 +0200

    feat(TargetChargingValueCalculationService): use separate service to calculate charging targets

---
+++ TeslaSolarCharger/Server/Services/Contracts/ITargetChargingValueCalculationService.cs
@@ -0,0 +1,10 @@
+﻿using TeslaSolarCharger.Server.Dtos.ChargingServiceV2;
+
+namespace TeslaSolarCharger.Server.Services.Contracts;
+
+public interface ITargetChargingValueCalculationService
+{
+    Task SetTargetValues(List<DtoTargetChargingValues> targetChargingValues,
+        List<DtoChargingSchedule> activeChargingSchedules, DateTimeOffset currentDate, int powerToControl,
+        CancellationToken cancellationToken);
+}
