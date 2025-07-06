99602af186c62cceebba81686ed6cee840fab164
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-07-06 22:54:19 +0200

    refactor(TeslaSolarChargerContext): add comment

--- TeslaSolarCharger.Model/EntityFramework/TeslaSolarChargerContext.cs
+++ TeslaSolarCharger.Model/EntityFramework/TeslaSolarChargerContext.cs
@@ -43,6 +43,7 @@ public class TeslaSolarChargerContext : DbContext, ITeslaSolarChargerContext
 
     public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
     {
+        //Workaround for https://github.com/dotnet/efcore/issues/29514
         await Database.ExecuteSqlRawAsync("PRAGMA busy_timeout=5000;", cancellationToken: cancellationToken);
         return await base.SaveChangesAsync(cancellationToken);
     }
