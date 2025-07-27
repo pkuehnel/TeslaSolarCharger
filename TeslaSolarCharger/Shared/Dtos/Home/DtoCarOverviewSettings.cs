aca6e0d0a2e212a787b1dbf75adcded8b295bb1b
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-07-27 12:52:50 +0200

    feat(CarDetailsComponent): add additional tool tips

--- TeslaSolarCharger/Shared/Dtos/Home/DtoCarOverviewSettings.cs
+++ TeslaSolarCharger/Shared/Dtos/Home/DtoCarOverviewSettings.cs
@@ -1,4 +1,5 @@
-﻿using TeslaSolarCharger.Shared.Enums;
+﻿using TeslaSolarCharger.Shared.Attributes;
+using TeslaSolarCharger.Shared.Enums;
 
 namespace TeslaSolarCharger.Shared.Dtos.Home;
 
@@ -10,7 +11,11 @@ public class DtoCarOverviewSettings
     }
 
     public string Name { get; set; }
+    [HelperText("Always charge at full speed until this soc even if there is not enough solar power")]
+    [Postfix("%")]
     public int? MinSoc { get; set; }
+    [HelperText("Stop charging at this soc even if there is enough solar power")]
+    [Postfix("%")]
     public int? MaxSoc { get; set; }
     public ChargeModeV2 ChargeMode { get; set; }
 }
