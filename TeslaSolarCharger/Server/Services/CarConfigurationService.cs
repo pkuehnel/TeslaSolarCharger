da57f718627e022f6157eb431de15bec0212d93f
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-07-28 20:23:40 +0200

    feat(HomeRazor): add info if no cars are enabled

--- TeslaSolarCharger/Server/Services/CarConfigurationService.cs
+++ TeslaSolarCharger/Server/Services/CarConfigurationService.cs
@@ -78,7 +78,7 @@ public class CarConfigurationService(ILogger<CarConfigurationService> logger,
                 MaximumAmpere = 16,
                 MinimumAmpere = 2,
                 UsableEnergy = 75,
-                ShouldBeManaged = true,
+                ShouldBeManaged = false,
                 IsAvailableInTeslaAccount = true,
                 ChargingPriority = ++highestChargingPriority,
             };
