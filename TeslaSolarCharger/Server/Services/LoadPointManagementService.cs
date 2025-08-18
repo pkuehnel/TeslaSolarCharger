c08f6a9cf5afcab90448a01011e969b437b22884
Patrick Kühnel <patrick.kuehnel96@gmail.com>
2025-08-18 15:34:47 +0200

    feat(LoadPointManagementService): add logging for last websocket connection

--- TeslaSolarCharger/Server/Services/LoadPointManagementService.cs
+++ TeslaSolarCharger/Server/Services/LoadPointManagementService.cs
@@ -93,7 +93,8 @@ public class LoadPointManagementService : ILoadPointManagementService
             IsOnline = car.IsOnline.Value,
         };
         var webSocketConnectedSince = _fleetTelemetryWebSocketService.ClientConnectedSince(car.Vin);
-        if(webSocketConnectedSince != default)
+        _logger.LogTrace("{method}({carId}) - WebSocket connected since: {webSocketConnectedSince}", nameof(GetCarOverviewState), carId, webSocketConnectedSince);
+        if (webSocketConnectedSince != default)
         {
             if (((webSocketConnectedSince.Value.AddMinutes(10) > _dateTimeProvider.DateTimeOffSetUtcNow()) && (car.IsOnline.Value == true))
                 || ((car.IsOnline.Value == false) && (car.IsOnline.LastChanged < webSocketConnectedSince.Value.AddMinutes(10))
