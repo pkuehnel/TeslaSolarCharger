9c5c7fb335c2ba433a2d8eb1839d97c93a491ada
pkuehnel <35361981+pkuehnel@users.noreply.github.com>
2025-10-13 20:08:01 +0200

    Add localization to additional client components

---
+++ TeslaSolarCharger/Shared/Localization/Registries/Components/CustomIconLocalizationRegistry.cs
@@ -0,0 +1,13 @@
+using TeslaSolarCharger.Shared.Localization;
+
+namespace TeslaSolarCharger.Shared.Localization.Registries.Components;
+
+public class CustomIconLocalizationRegistry : TextLocalizationRegistry<CustomIconLocalizationRegistry>
+{
+    protected override void Configure()
+    {
+        Register("Not ",
+            new TextLocalizationTranslation(LanguageCodes.English, "Not "),
+            new TextLocalizationTranslation(LanguageCodes.German, "Nicht "));
+    }
+}
