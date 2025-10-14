ffa321b7f54c217eb580fd38e7db3733ba1db02f
pkuehnel <35361981+pkuehnel@users.noreply.github.com>
2025-10-14 19:43:47 +0200

    Fix singular and plural localization for not charging reasons

--- TeslaSolarCharger/Shared/Localization/Registries/Components/StartPage/NotChargingAtExpectedPowerReasonsComponentLocalizationRegistry.cs
+++ TeslaSolarCharger/Shared/Localization/Registries/Components/StartPage/NotChargingAtExpectedPowerReasonsComponentLocalizationRegistry.cs
@@ -6,8 +6,12 @@ public class NotChargingAtExpectedPowerReasonsComponentLocalizationRegistry : Te
 {
     protected override void Configure()
     {
-        Register("{0} reason(s) why loadpoint charges with different power than you might expect",
-            new TextLocalizationTranslation(LanguageCodes.English, "{0} reason(s) why loadpoint charges with different power than you might expect"),
+        Register("{0} reason why loadpoint charges with different power than you might expect",
+            new TextLocalizationTranslation(LanguageCodes.English, "{0} reason why loadpoint charges with different power than you might expect"),
+            new TextLocalizationTranslation(LanguageCodes.German, "{0} Grund, warum der Ladepunkt mit einer anderen Leistung lädt als erwartet"));
+
+        Register("{0} reasons why loadpoint charges with different power than you might expect",
+            new TextLocalizationTranslation(LanguageCodes.English, "{0} reasons why loadpoint charges with different power than you might expect"),
             new TextLocalizationTranslation(LanguageCodes.German, "{0} Gründe, warum der Ladepunkt mit einer anderen Leistung lädt als erwartet"));
 
         Register("remaining)",
