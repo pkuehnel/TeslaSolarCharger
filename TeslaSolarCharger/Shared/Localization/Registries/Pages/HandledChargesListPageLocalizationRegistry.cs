18f86af1a0809047512ab5a99fd24ecdcbe5eb7f
pkuehnel <35361981+pkuehnel@users.noreply.github.com>
2025-10-15 12:13:22 +0200

    Translate handled charges filters

--- TeslaSolarCharger/Shared/Localization/Registries/Pages/HandledChargesListPageLocalizationRegistry.cs
+++ TeslaSolarCharger/Shared/Localization/Registries/Pages/HandledChargesListPageLocalizationRegistry.cs
@@ -9,5 +9,21 @@ public class HandledChargesListPageLocalizationRegistry : TextLocalizationRegist
         Register("Handled Charges",
             new TextLocalizationTranslation(LanguageCodes.English, "Handled Charges"),
             new TextLocalizationTranslation(LanguageCodes.German, "Verarbeitete Ladevorgänge"));
+
+        Register("Hide charging processes with known cars",
+            new TextLocalizationTranslation(LanguageCodes.English, "Hide charging processes with known cars"),
+            new TextLocalizationTranslation(LanguageCodes.German, "Ladevorgänge mit bekannten Fahrzeugen ausblenden"));
+
+        Register("Hide charging processes where the car is known",
+            new TextLocalizationTranslation(LanguageCodes.English, "Hide charging processes where the car is known"),
+            new TextLocalizationTranslation(LanguageCodes.German, "Ladevorgänge ausblenden, bei denen das Fahrzeug bekannt ist"));
+
+        Register("Minimum consumed energy",
+            new TextLocalizationTranslation(LanguageCodes.English, "Minimum consumed energy"),
+            new TextLocalizationTranslation(LanguageCodes.German, "Minimal verbrauchte Energie"));
+
+        Register("Hide charging processes where less energy is consumed",
+            new TextLocalizationTranslation(LanguageCodes.English, "Hide charging processes where less energy is consumed"),
+            new TextLocalizationTranslation(LanguageCodes.German, "Ladevorgänge ausblenden, bei denen weniger Energie verbraucht wurde"));
     }
 }
