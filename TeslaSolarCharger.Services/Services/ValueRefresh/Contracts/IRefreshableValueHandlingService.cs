37d55d623a561c84b94eb752eb48192654ed26d4
pkuehnel <35361981+pkuehnel@users.noreply.github.com>
2025-10-18 14:33:43 +0200

    Handle refreshable errors when updating PV values

--- TeslaSolarCharger.Services/Services/ValueRefresh/Contracts/IRefreshableValueHandlingService.cs
+++ TeslaSolarCharger.Services/Services/ValueRefresh/Contracts/IRefreshableValueHandlingService.cs
@@ -1,10 +1,10 @@
-﻿using TeslaSolarCharger.SharedModel.Enums;
+using TeslaSolarCharger.SharedModel.Enums;
 
 namespace TeslaSolarCharger.Services.Services.ValueRefresh.Contracts;
 
 public interface IRefreshableValueHandlingService
 {
-    IReadOnlyDictionary<ValueUsage, decimal> GetSolarValues();
+    IReadOnlyDictionary<ValueUsage, decimal> GetSolarValues(out bool hasErrors);
     Task RecreateRefreshables();
     Task RefreshValues();
 }
